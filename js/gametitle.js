d08a639cdf7f1dd4aad8d01d5d7fdb62422d938e
Krzysztof Andrelczyk <kandrelczyk@theworkshop.com>
2017-07-29 20:17:16 +0200

    shot anim

--- js/gametitle.js
+++ js/gametitle.js
@@ -4,9 +4,10 @@ gameTitle.prototype = {
     create: function(){
         var gameTitle = this.game.add.sprite(this.game.width/2,this.game.height/2,"gametitle");
         gameTitle.anchor.setTo(0.5,0.5);
-        gameTitle.scale.setTo(0.75, 0.4);
-        var playButton = this.game.add.button(330,220,"play",this.playTheGame,this);
+        gameTitle.scale.setTo(0.22, 0.22);
+        var playButton = this.game.add.button(800,this.game.height/2,"play",this.playTheGame,this);
         playButton.anchor.setTo(0.5,0.5);
+        playButton.scale.setTo(0.4, 0.4);
 
         this.menuMusic = this.game.add.audio('menu');
         this.menuMusic.loopFull();
