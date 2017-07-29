b183f7e85fc5e9efeece9a6d6de6426e334b14a5
EdwardYolag <benjamin.belmont.p@gmail.com>
2017-07-29 14:05:46 +0200

    Fixed Shooting 2

--- js/thegame.js
+++ js/thegame.js
@@ -76,6 +76,43 @@ theGame.prototype = {
         //  Our controls.
         cursors = this.game.input.keyboard.createCursorKeys();
 
+
+
+        // PLAYER 2 implantation:
+
+        // The player2 and its settings
+        player2 = this.game.add.sprite(50, 50, 'atom');
+
+        player2.charge = -1;
+        player2.checkWorldBounds = true;
+        player2.events.onOutOfBounds.add(this.gameOver, this);
+
+        player2.scale.setTo(0.8, 0.8);
+        //  We need to enable physics on the player
+        this.game.physics.arcade.enable(player2);
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        player2.body.bounce.y = 0;
+        player2.body.gravity.y = 1;
+        player2.body.collideWorldBounds = false;
+
+        //  Our two animations, walking left and right.
+        player2.animations.add('idle_b', [5, 6, 7, 8, 9, 10, 11, 12], 10, true);
+        player2.animations.add('idle_r', [13, 14, 15, 16, 17, 18, 19, 20], 10, true);
+
+        //  Finally some stars to collect
+        stars = this.game.add.group();
+
+        //  We will enable physics for any star that is created in this group
+        stars.enableBody = true;
+
+        player2.animations.play('idle_b');
+
+
+        
+
+        // PLAYER 2 FINISH.
+
     },
 
     update: function () {
@@ -110,11 +147,6 @@ theGame.prototype = {
                 this.lastShot = Date.now();
 
 
-
-                
-
-                
-
                 //  Let gravity do its thing
                 
 
@@ -122,36 +154,40 @@ theGame.prototype = {
                 if (cursors.down.isDown && cursors.left.isDown) {
                     var star = stars.create(player.body.x + (50 * direction) + 30, player.body.y + 40, 'star');
                     star.body.velocity.y = 300;
+                    console.log("SHOOT DOWN LEFT")
                 }
                 else if (cursors.down.isDown && cursors.right.isDown) {
-                    var star = stars.create(player.body.x + (50 * direction) - 30, player.body.y + 40, 'star');
+                    var star = stars.create(player.body.x + (80 * direction), player.body.y + 40, 'star');
                     star.body.velocity.y = 300;
+                    console.log("SHOOT DOWN RIGHT")
                 }
                 else if (cursors.up.isDown && cursors.left.isDown) {
                     var star = stars.create(player.body.x + (50 * direction) + 30, player.body.y, 'star');
                     star.body.velocity.y = -300;
+                    console.log("SHOOT UP LEFT")
                 }
                 else if (cursors.up.isDown && cursors.right.isDown) {
-                    var star = stars.create(player.body.x + (50 * direction) - 30, player.body.y, 'star');
+                    var star = stars.create(player.body.x + (80 * direction), player.body.y, 'star');
                     star.body.velocity.y = -300;
+                    console.log("SHOOT UP RIGHT")
                 }
 
                 else if (cursors.down.isDown) {
-                    var star = stars.create(player.body.x + (40 * direction) + 3.5, player.body.y + 50, 'star');
+                    var star = stars.create(player.body.x + (40 * direction) + 25, player.body.y + 60, 'star');
                     star.body.velocity.y = 300;
                     console.log("shoot DOWN");
                 }
                 else if (cursors.up.isDown) {
-                    var star = stars.create(player.body.x + (40 * direction) + 3.5, player.body.y - 20, 'star');
+                    var star = stars.create(player.body.x + (40 * direction) + 25, player.body.y - 20, 'star');
                     star.body.velocity.y = -300;
                     console.log("shoot UP");
                 }
                 else if (cursors.left.isDown) {
-                    var star = stars.create(player.body.x + (20 * direction), player.body.y + 10, 'star');
+                    var star = stars.create(player.body.x + (20 * direction), player.body.y + 15, 'star');
                     console.log("shoot LEFT");
                 }
                 else if (cursors.right.isDown) {
-                    var star = stars.create(player.body.x + (30 * direction), player.body.y + 10, 'star');
+                    var star = stars.create(player.body.x + (80 * direction), player.body.y + 15, 'star');
                     console.log("shoot RIGHT");
                 }
                 
@@ -174,10 +210,15 @@ theGame.prototype = {
         }
 
         platforms.forEach(this.checkAttracion, this, true, player);
+        platforms.forEach(this.checkAttracion, this, true, player2);
 
         player.magneticVelocityY = player.body.velocity.y;
         player.magneticVelocityX = player.body.velocity.x;
         this.game.physics.arcade.collide(player, platforms, this.collidePlatform, null, this);
+
+        player2.magneticVelocityY = player2.body.velocity.y;
+        player2.magneticVelocityX = player2.body.velocity.x;
+        this.game.physics.arcade.collide(player2, platforms, this.collidePlatform, null, this);
 
     },
 
@@ -209,7 +250,7 @@ theGame.prototype = {
         var platformX = platform.body.x;
         var platformY = platform.body.y;
 
-        console.log("width: " + platformW + " height: " + platformH);
+        
         
 
         var playerX = player.body.x;
@@ -292,7 +333,20 @@ theGame.prototype = {
             player.body.velocity.y = player.magneticVelocityY;
 
         }
+
+        if (player2.charge != platform.charge) {
+
+            player2.body.velocity.x = 0;
+            player2.body.velocity.y = 0;
+        } else {
+            player2.body.velocity.x = player.magneticVelocityX;
+            player2.body.velocity.y = player.magneticVelocityY;
+
+        }
     },
+
+
+
 
     gameOver: function (player) {
         this.gameMusic.stop();
