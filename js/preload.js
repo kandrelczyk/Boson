a1f64866efd2de7e0e5846980ae3412b38e88cb6
EdwardYolag <benjamin.belmont.p@gmail.com>
2017-07-29 16:02:41 +0200

    merging platformsupdate

--- js/preload.js
+++ js/preload.js
@@ -8,10 +8,15 @@ preload.prototype = {
         this.game.load.image('star', 'assets/star.png');
         this.game.load.spritesheet('atom', 'assets/atom.png', 98, 78);
         this.game.load.spritesheet('atom2', 'assets/atom2.png', 98, 78);
+        this.game.load.spritesheet('platform1', 'assets/platform1_bNr', 400, 45);
+        this.game.load.spritesheet('platform2', 'assets/platform2_bNr', 400, 119);
+        this.game.load.spritesheet('platform3', 'assets/platform3_bNr', 400, 137);
 
         this.game.load.audio('menu', 'assets/audio/menu.mp3');
         this.game.load.audio('game', 'assets/audio/game.mp3');
         this.game.load.image('verticalground', 'assets/verticalplatform.png');
+
+
     },
 
     create: function(){
