be923801dc150a0642b758081a7992f6be51c930
EdwardYolag <benjamin.belmont.p@gmail.com>
2017-07-30 12:11:57 +0200

    definitive background

--- js/preload.js
+++ js/preload.js
@@ -4,7 +4,7 @@ preload.prototype = {
     preload: function(){
         this.game.load.image('gametitle', 'assets/gametitle.png');
         this.game.load.image('play', 'assets/play.png');
-        this.game.load.image('background', 'assets/BACKGROUND_40.png');
+        this.game.load.image('background', 'assets/BACKGROUND_DESATURADO_50.png');
         this.game.load.image('ground', 'assets/platform.png');
         this.game.load.image('credits', 'assets/CREDITOS.png');
         this.game.load.spritesheet('bullet', 'assets/bullet.png', 8, 8);
